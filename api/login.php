1e5bc4602e19bdb3db28001c0556ad6b757d5d03
pacobarbe31 <paco.barbe@limayrac.fr>
2019-05-07 15:07:16 +0200

    Correction du JSON

--- api/login.php
+++ api/login.php
@@ -5,21 +5,16 @@
 // Authentifie un client Android et renvoie une réponse JSON
 // Exemple : http://http://localhost/BTS/PPE-G2/api/login.php?user=vinz@email.com&password=vinzvinz
 include "../config/init.php";
-
-
 $adherentDAO = new AdherentDAO();
-$notefraisDAO = new NoteFraisDAO();
+$notefraisDAO = new NotefraisDAO();
 $lignefraisDAO = new LignefraisDAO();
 $indemniteDAO = new IndemniteDAO();
 $motifDAO = new MotifDAO();
 $clubDAO = new ClubDAO();
-
 $authentifie = false;
-
 // Récupère les paramètres dans l'URL
 $user = isset($_GET["user"]) ? $_GET["user"] : "";
 $password = isset($_GET["password"]) ? $_GET["password"] : "";
-
 //On vérifie que le mail et mdp soient correct et on stocke les infos de l'adh majeur dans la variable $UserConnected
 	if ($adherentDAO->est_inscrit($user, $password)){
             $UserConnected = $adherentDAO->findByMail($user);
@@ -29,7 +24,6 @@ $password = isset($_GET["password"]) ? $_GET["password"] : "";
     }else{
         $authentifie = false;
     }
-
 // Si authentifié, fournit le bordereau de l'année en cours
 if ($authentifie) {
     
@@ -37,29 +31,30 @@ if ($authentifie) {
 
     foreach($notes as $UneNote) {
         $id_note_frais = $UneNote->getid_note_frais();
+    }
+        //$tableau_lignes = array();
 
-    // Remise a 0 du tableau
-    $tableau_lignes = array();
+        // Recupere les lignes de frais du bordereau
+        $lignes = $lignefraisDAO->find($id_note_frais);
 
-    // Recupere les lignes de frais du bordereau
-    $lignes = $lignefraisDAO->find($id_note_frais);
 
     foreach($lignes as $ligne){
-    
         $tableau_lignes[] = array( 
         //"Id borderau"=>$bordereauencour->get_ID_bordereau(),
-        "DateFrais "=>$ligne->getdate_frais(),
-        "Trajet"=>$ligne->gettrajet_frais(), 
-        "KM"=>$ligne->getkm_parcourus(), 
-        "Peages"=>$ligne->getCout_peage(), 
-        "CoutRepas"=>$ligne->getCout_repas(), 
-        "coutHebergement "=>$ligne->getCout_hebergement()
+        "trajet"=>$ligne->gettrajet_frais(),
+        "date "=>$ligne->getdate_frais(),
+        "km"=>$ligne->getkm_parcourus(), 
+        "peage"=>$ligne->getCout_peage(), 
+        "repas"=>$ligne->getCout_repas(), 
+        "heberg "=>$ligne->getCout_hebergement()
         //"Motif"=>$motifDAO->findMotifByIdMotif($ligne->get_IdMotif())->get_Libelle(),      
-        //"nom Club"=>$clubDAO->find($ligne->get_ID_club())->get_Nom_club()  
-    ); 
-
+        //"nom Club"=>$clubDAO->find($ligne->get_ID_club())->get_Nom_club() 
+        );
     }
-}
+        $tableau_bordereaux = array(
+        "lignes_frais" => $tableau_lignes
+        );
+
     
     //var_dump($tableau_lignes);
     //$tableau_lignes[] = (object) $ligne;
@@ -70,10 +65,10 @@ if ($authentifie) {
   }
   
 // Construit le format JSON
-$json = json_encode($tableau_lignes);
-//echo $json;
+$json = json_encode($tableau_bordereaux);
+echo $json;
+
 //$json2 = json_decode($json);
 //var_dump($json);
-
 // Envoie la réponse 
-send_json($json);
+//send_json($json);
